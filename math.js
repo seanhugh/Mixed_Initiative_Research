02b3041ab572bbf185110a5ccd069925dec5bc79
Eric Lu <penlume@gmail.com>
2018-11-13 09:43:26 -0500

    object-ize functions

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   var MQ = MathQuill.getInterface(2);
   MQ.StaticMath($('#problem')[0]);
-  var entry = MQ.MathField($('#entryBox')[0], {
+  MATH.entry = MQ.MathField($('#entryBox')[0], {
     handlers: {
       edit: function() {
         //checkAnswer(entry.latex());
@@ -11,25 +11,25 @@ $(document).ready(function() {
     }
   });
 
-  function saveEquation() {
+  MATH.saveEquation = function () {
     entry = MQ.MathField($('#entryBox')[0]);
     console.log(entry.latex());
   }
 
-  function makePreview() {
-    input = entry.latex().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  MATH.makePreview = function () {
+    input = MATH.entry.latex().replace(/</g, "&lt;").replace(/>/g, "&gt;");
     $('#preview').html("$$" + input + "$$");
     MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
   }
 
-  function screen2(){
-    saveEquation();
+  MATH.showControls = function () {
+    MATH.saveEquation();
     $('#entryWrapper').hide();
     $('#continueButton').hide();
     $('#preview').show();
     $('#controls').show();
-    makePreview();
+    MATH.makePreview();
   }
 
-  $('#continueButton').on('click', screen2);
+  $('#continueButton').on('click', MATH.showControls);
 });
