c8a41fd46dbaa2271135c9ee6ca30023aebd7b9a
Sean Hughes <shughes@college.harvard.edu>
2018-11-23 13:58:06 -0500

    static files loaded

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
-from flask import Flask
+### HOW TO KILL LOST APP ###
+
+# How to kill flask:
+# ps -ef | grep python
+# kill -9 ID
+
+### STRUCTURE OF FOLDER ###
+
+#/app
+#    - app.py
+#    /templates
+#        - index.html
+#    /static
+#        /css
+#            - styles.css
+#        /js
+#        /img
+
+### STRUCTURE OF THE JSON
+#
+# DISPLAY JSON:
+# eq: equation in latek
+# tree: the current tree
+# location: location within the tree
+# buttons: list of buttons (use IDS for buttons)
+#
+# ACTION JSON:
+# Button clicked: id of button clicked
+# Location in tree: location of cursor within tree
+
+from flask import Flask, render_template, request, url_for
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, world!\n'
+@app.route('/index')
+def show_index():
+    return render_template("index.html")
