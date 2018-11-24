c43c85ba6f0f8b4aa3d46d44d61efd32dacfe75e
Sean Hughes <shughes@college.harvard.edu>
2018-11-24 13:25:51 -0500

    flask backend merged

--- app.py
+++ app.py
@@ -33,7 +33,12 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def show_index():
+  if request.method == 'GET':
     return render_template("index.html")
+  else:
+    myData = request.form['equation']
+    return render_template("equation.html", data = myData)
+    # return render_template("equation.html", data = myData)
+
--- static/js/math.js
+++ static/js/math.js
@@ -1,3 +1,27 @@
+  function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+
 $(document).ready(function() {
   window.MATH = {};
 
@@ -13,22 +37,34 @@ $(document).ready(function() {
 
   MATH.saveEquation = function () {
     entry = MQ.MathField($('#entryBox')[0]);
-    console.log(entry.latex());
   }
+
+  // MATH.makePreview = function () {
+  //   input = MATH.entry.latex().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  //   $('#preview').html("$$" + input + "$$");
+  //   MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
+  // }
 
   MATH.makePreview = function () {
     input = MATH.entry.latex().replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    $('#preview').html("$$" + input + "$$");
-    MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
+    input = ("$$" + input + "$$");
+    return input
   }
+
+
 
   MATH.showControls = function () {
     MATH.saveEquation();
-    $('#entryWrapper').hide();
-    $('#continueButton').hide();
-    $('#preview').show();
-    $('#controls').show();
-    MATH.makePreview();
+    // $('#entryWrapper').hide();
+    // $('#continueButton').hide();
+    //$('#preview').show();
+    //$('#controls').show();
+
+    myEq = MATH.makePreview();
+
+    post('/', {equation:  myEq});
+
+    // window.location.href = '/equation';
   }
 
   $('#continueButton').on('click', MATH.showControls);
