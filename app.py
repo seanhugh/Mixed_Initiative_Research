fec1af5cfda3a5efd9401b0a4a2998ebe4eff3ed
Sean Hughes <shughes@college.harvard.edu>
2018-12-11 23:27:33 -0500

    workflow additions made. Randomization updated.

--- app.py
+++ app.py
@@ -71,20 +71,10 @@ def makeState(equation, active):
 # To add in equations just edit the equations here, randomization will be
 #  done automatically:
 
-equations = [["1x", "2x"], ["3x", "4x"]]
-
-rand_list = [1,0]
-
-shuffle(rand_list)
 
 active_list = [True, False]
 
 shuffle(active_list)
-
-states = [makeState(equations[rand_list[0]][0], active_list[0]),
-          makeState(equations[rand_list[0]][1], active_list[0]),
-          makeState(equations[rand_list[1]][0], active_list[1]),
-          makeState(equations[rand_list[1]][1], active_list[1])]
 
 # Set the order of equations
 
@@ -95,9 +85,9 @@ states = [makeState(equations[rand_list[0]][0], active_list[0]),
 def show_survey(path):
 
   if path == "beg":
-    text = "This is some sample instructions"
-    title = "Survey " + path
-    link_loc = "/text/intro1"
+    text = "Thank you for participating in our research project! Please answer ALL of the below questions, submit the form, then proceed to the next page. Thanks!"
+    title = "Introduction Survey"
+    link_loc = "/text/vid1"
     data = {"text": text,
               "title": title,
               "link": link_loc}
@@ -107,13 +97,13 @@ def show_survey(path):
   title = "Survey " + path
   if path == "1" :
     link_loc = "/text/intro2"
-    if states[1]["active"] == True:
+    if active_list[0] == True:
       form_link = "https://docs.google.com/forms/d/e/1FAIpQLSdu4JIAQMew4SEO9UQ_5udRfrnraWr7CaGfJBtxHJucy_SANA/viewform?embedded=true"
     else:
       form_link = "https://docs.google.com/forms/d/e/1FAIpQLSel3Nkn2CfxoEHevEl63i6_zaT4jt-tUzfKCVz4H68fkZ9Ifg/viewform?embedded=true"
   else:
     link_loc = "/text/fin"
-    if states[3]["active"] == False:
+    if active_list[1] == False:
       form_link = "https://docs.google.com/forms/d/e/1FAIpQLSel3Nkn2CfxoEHevEl63i6_zaT4jt-tUzfKCVz4H68fkZ9Ifg/viewform?embedded=true"
     else:
       form_link = "https://docs.google.com/forms/d/e/1FAIpQLSdu4JIAQMew4SEO9UQ_5udRfrnraWr7CaGfJBtxHJucy_SANA/viewform?embedded=true"
@@ -128,6 +118,15 @@ def show_survey(path):
 # A text Screen
 @app.route('/text/<part>', methods=['GET'])
 def show_text(part):
+    if(part == "vid1"):
+      text = "In this experiment we will be comparing two different equation solving tools. We will provide 2 different problems to solve for each tool. You are allowed to use pen & paper alongside with the tools. Below is a video that demonstrates how to interact with these tools:"
+
+      data = {"text": text,
+              "title": "Experiment Explanation",
+              "link": "/text/intro1"}
+
+      return render_template('text.html', data = data)
+
     if(part == "intro1"):
       #DO THE INTRO STUFF
       # continue button: 1
@@ -191,8 +190,7 @@ def show_1(part):
       if(part in ["1","2","3","4"]):
         #DO THE EQUATION SOLVING STUFF
         # Set the equation
-        cur_state = states[int(part) - 1]
-        startState = model.init(cur_state["equation"], cur_state["active"])
+        startEquation = request.form['equation']
 
         # Set the title for the top of the screen
         title = part
@@ -200,12 +198,18 @@ def show_1(part):
         # Set the link location
         if part == "1":
           link_loc = "/experiment/2"
+          isActive = active_list[0]
         elif part == "2":
           link_loc = "/survey/1"
+          isActive = active_list[0]
         elif part == "3":
           link_loc = "/experiment/4"
+          isActive = active_list[1]
         elif part == "4":
           link_loc = "/survey/2"
+          isActive = active_list[1]
+
+        startState = model.init(startEquation, isActive)
 
         data = {"title": title,
                 "link": link_loc}
